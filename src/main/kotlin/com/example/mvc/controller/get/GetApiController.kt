5ba8d7371dafe4c914a8491725c940a9a4e62e88
ehjeong <ehjeong@hexaveil.com>
2023-05-01 18:58:06 +0900

    RESTAPI서버

---
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -0,0 +1,55 @@
+package com.example.mvc.controller.get
+
+import com.example.mvc.controller.model.http.UserRequest
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController   //REST APIController로 동작
+@RequestMapping("/api")
+class GetApiController {
+    @GetMapping("/hello")
+    fun hello():String{
+        return "hellow kotlin"
+    }
+    @RequestMapping(method = [RequestMethod.GET], path = ["/request-mapping"])
+    fun requestMappig(): String {
+        return "request-mapping"
+    }
+    @GetMapping("/get-mapping/path-variable/{name}/{age}")
+    fun pathVariable(@PathVariable name:String,@PathVariable age:Int): String {
+        println("${name},${age}")
+        return name+""+age
+    }
+    @GetMapping("/get-mapping/path-variable2/{name}/{age}")
+    fun pathVariable2(@PathVariable(value = "name") _name:String,@PathVariable(value = "age") _age:Int): String {
+        println("${_name},${_age}")
+        return _name+""+_age
+    }
+
+    @GetMapping("/get-mapping/query-param")
+    fun queryParam(
+            @RequestParam name:String,
+            @RequestParam age:Int
+    ): String {
+        println("${name},${age}")
+        return name+""+age
+    }
+    
+    @GetMapping("/get-mapping/obj")
+    fun queryParamObj(userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+
+        
+    }
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map: Map<String,Any>): Map<String, Any> {
+        println(map)
+        return map
+
+    }
+}
---
+++ src/main/kotlin/com/example/mvc/controller/model/http/UserRequest.kt
@@ -0,0 +1,8 @@
+package com.example.mvc.controller.model.http
+
+data class UserRequest (
+        var name: String,
+        var age:Int,
+        var email:String,
+        var address:String,
+)
